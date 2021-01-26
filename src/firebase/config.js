71a314af4fc8ba300bca00d7f3c4710b51ef1f73
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-01-25 20:47:38 -0400

    Inicialización Firebase
    Archivo de configuración

---
+++ src/firebase/config.js
@@ -0,0 +1,29 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+// Inicializa Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyB-D5GKthp-gSLwUeBxk78-qpkoIeA81n8",
+  authDomain: "inmobily-dc783.firebaseapp.com",
+  projectId: "inmobily-dc783",
+  storageBucket: "inmobily-dc783.appspot.com",
+  messagingSenderId: "1059413922941",
+  appId: "1:1059413922941:web:22e0958f06712becab8d14",
+});
+
+// Autenticación
+const auth = firebase.auth();
+// Firestore (Base de datos)
+const db = firebase.firestore();
+// Analytics
+const analytics = firebase.analytics();
+
+// Persistencia sin conexión
+db.enablePersistence().catch(function (err) {
+  // TODO: Manejar mejor el error
+  console.log(err.message);
+});
+
+export { auth, db, analytics };
